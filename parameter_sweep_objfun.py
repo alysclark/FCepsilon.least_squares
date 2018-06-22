dab18ea778c3259aeb36294b33a6eea7f559f45a
Alys Clark <alys.clark@auckland.ac.nz>
2018-06-22 13:36:52 +1200

    setting up meaningful parameter ranges for sweep

--- parameter_sweep_objfun.py
+++ parameter_sweep_objfun.py
@@ -9,11 +9,12 @@ from matplotlib import pyplot as plt
 import OpenCOR as oc
 
 #Some example output that we are maybe aiming for
-times = np.array([0, 240, 480, 960, 1920, 3840])
-pFC = np.array([0.0, 0.0189, 0.0208, 0.0646, 0.0495, 0.0645])
-pSyk = np.array([0.0, 0.0143, 0.0255, 0.0303, 0.0242, 0.0202])
+times = np.array([0,  480, 960, 1920, 3840])
+pFC = np.array([0.0,  0.0208, 0.0646, 0.0495, 0.0645])
+pSyk = np.array([0.0,  0.0255, 0.0303, 0.0242, 0.0202])
 
-
+bounds_dictionary = {'FCepsilonRI/k_f1': [-4,2], 'FCepsilonRI/k_r1': [-3,1], 'FCepsilonRI/k_f2': [-4,2], 'FCepsilonRI/k_f3': [-4,2],
+        'FCepsilonRI/k_r3': [-3,1], 'FCepsilonRI/k_f4': [-4,2], 'FCepsilonRI/k_f5': [-4,2], 'FCepsilonRI/k_r5': [-3,1],'FCepsilonRI/k_f6': [-4,2]}
 class Simulation(object):
     def __init__(self):
         self.simulation = oc.simulation()
@@ -24,14 +25,15 @@ class Simulation(object):
         self.model_constants = OrderedDict({k: self.constants[k]
                                             for k in self.constants.keys()})
         
+        print(self.model_constants)
+        
         # default the parameter bounds to something sensible, needs to be set directly
         bounds = []
-        scale = 10.0
         for c in self.constants:
             v = self.constants[c];
-            print(c)
-            print(v)
-            bounds.append([v/scale, v*scale])
+            print(bounds_dictionary[c][1])
+            #print(v)
+            bounds.append([bounds_dictionary[c][0], bounds_dictionary[c][1]])
         # define our sensitivity analysis problem
         self.problem = {
                    'num_vars': len(self.constants),
@@ -39,14 +41,14 @@ class Simulation(object):
                    'bounds': bounds
                    }
         self.samples = saltelli.sample(self.problem, 3)
-
+    
     def run_once(self, c, v):
         self.simulation.resetParameters()
         self.constants[c] = v
         self.simulation.run()
         return (self.simulation.results().points().values(),
-                self.simulation.results().states()['FCepsilonRI/pGrb2'].values())
-
+                self.simulation.results().states()['FCepsilonRI/pSyk'].values())
+    
     def run(self, c, scale=2.0):
         self.simulation.clearResults()
         v = self.model_constants[c]
@@ -56,7 +58,7 @@ class Simulation(object):
             trial = self.run_once(c, s*v)[1][times]
             divergence += math.sqrt(np.sum((base - trial)**2))
         return divergence
-
+    
     def evaluate_model(self, parameter_values):
         self.simulation.clearResults()
         for i, k in enumerate(self.model_constants.keys()):
@@ -64,20 +66,21 @@ class Simulation(object):
         #print('Parameter set: ', parameter_values)
         self.simulation.run()
         return (self.simulation.results().states()['FCepsilonRI/pSyk'].values()[times])
-        
+    
     def evaluate_ssq(self, parameter_values):
         self.simulation.clearResults()
+		#This is not actually clearing and resetting results
         for i, k in enumerate(self.model_constants.keys()):
-            self.constants[k] = parameter_values[i]
-        #print('Parameter set: ', parameter_values)
+            self.constants[k] = 10.0**parameter_values[i]
+        print('Parameter set: ', self.constants)
         self.simulation.run()
         trial_pSyk = self.simulation.results().states()['FCepsilonRI/pSyk'].values()[times]
         ssq_pSyk = math.sqrt(np.sum((pSyk-trial_pSyk)**2))
-		
+        
         trial_pFC = self.simulation.results().states()['FCepsilonRI/pFC'].values()[times]
         ssq_pFC = math.sqrt(np.sum((pFC-trial_pFC)**2))
-		
-        plt.plot(times,trial_pFC)
+        
+        plt.plot(times,trial_pSyk)
         #ssq=0.0
         #for i in range(0,len(differ)):
         #    ssq = ssq+differ[i]**2
@@ -85,17 +88,17 @@ class Simulation(object):
         print(ssq_pSyk,ssq_pFC)
         return(ssq_pSyk)
         
-        
+    
     def run_parameter_sweep(self):
         Y = np.zeros([self.samples.shape[0]])
         for i, X in enumerate(self.samples):
             Y[i] = self.evaluate_ssq(X)
-
+        
         return Y
 
 
 s = Simulation()
 
 v = s.run_parameter_sweep()
-
+plt.plot(times,pSyk,'*')
 plt.show()
