74dfcc53720a985e75865e2c1f2321fee0ef7c13
Alys Clark <alys.clark@auckland.ac.nz>
2018-07-01 14:12:38 +1200

    add opportunity to plot n best results including relationship between errors in two objectives

--- parameter_sweep_objfun.py
+++ parameter_sweep_objfun.py
@@ -13,11 +13,11 @@ times = np.array([0,  480, 960, 1920, 3840])
 pFC = np.array([0.0, 0.0408, 0.136, 0.105, 0.136])*.474
 pSyk = np.array([0.0,  0.05437, 0.0644, 0.0518, 0.04373])*.474
 
-bounds_dictionary = {'FCepsilonRI/V_1': [-6,3], 'FCepsilonRI/k_f5': [-6,3], 'FCepsilonRI/K_1': [-6,3], 'FCepsilonRI/V_2': [-6,3],
-    'FCepsilonRI/K_2': [-6,3], 'FCepsilonRI/k_f6': [-6,3], 'FCepsilonRI/k_f4': [-6,3], 'FCepsilonRI/k_f3': [-6,3], 'FCepsilonRI/k_r1': [-6,3], 'FCepsilonRI/k_r3': [-6,3],
-	'FCepsilonRI/k_f2': [-6,3], 'FCepsilonRI/Lyn': [-6,3], 'FCepsilonRI/K_3': [-6,3], 'FCepsilonRI/K_4': [-6,3], 'FCepsilonRI/k_f1': [-6,3], 'FCepsilonRI/K_6': [-6,3], 
-	'FCepsilonRI/K_5': [-6,3], 'FCepsilonRI/k_f7': [-6,3]}
-	
+bounds_dictionary = {'FCepsilonRI/k_f1': [-5,2], 'FCepsilonRI/k_f2': [-5,2],'FCepsilonRI/k_f3': [-5,2], 'FCepsilonRI/k_f4': [-5,2],'FCepsilonRI/K_1': [-5,2], 'FCepsilonRI/K_2': [-5,2],'FCepsilonRI/K_3': [-5,2],
+	'FCepsilonRI/K_4': [-5,2], 'FCepsilonRI/K_5': [-5,2],'FCepsilonRI/K_6': [-5,2], 'FCepsilonRI/K_7': [-5,2],'FCepsilonRI/K_8': [-5,2], 'FCepsilonRI/K_9': [-5,2],'FCepsilonRI/K_10': [-5,2], 'FCepsilonRI/K_11': [-5,2],
+	'FCepsilonRI/K_12': [-5,2],'FCepsilonRI/V_1': [-5,2],'FCepsilonRI/V_2': [-5,2],'FCepsilonRI/V_3': [-5,2],'FCepsilonRI/V_4': [-5,2], 'FCepsilonRI/pLyn': [-1.32640456,-1.32640455]}
+
+
 class Simulation(object):
     def __init__(self):
         self.simulation = oc.simulation()
@@ -32,10 +32,12 @@ class Simulation(object):
         
         # default the parameter bounds to something sensible, needs to be set directly
         bounds = []
+        print(self.constants)
         for c in self.constants:
             v = self.constants[c];
-            #print(bounds_dictionary[c][1])
-            #print(v)
+            print(c)
+            print(bounds_dictionary[c][1])
+            print(v)
             bounds.append([bounds_dictionary[c][0], bounds_dictionary[c][1]])
         # define our sensitivity analysis problem
         self.problem = {
@@ -43,7 +45,7 @@ class Simulation(object):
                    'names': self.constants.keys(),
                    'bounds': bounds
                    }
-        self.samples = saltelli.sample(self.problem, 1)
+        self.samples = saltelli.sample(self.problem, 50)
     
     def run_once(self, c, v):
         self.simulation.resetParameters()
@@ -81,14 +83,15 @@ class Simulation(object):
         trial_pSyk = self.simulation.results().states()['FCepsilonRI/pSyk'].values()[times]
         ssq_pSyk = math.sqrt(np.sum((pSyk-trial_pSyk)**2))
         
-        trial_pFC = self.simulation.results().states()['FCepsilonRI/pFC'].values()[times] #+ self.simulation.results().states()['FCepsilonRI/pFCLyn'].values()[times] + self.simulation.results().states()['FCepsilonRI/pFCSyk'].values()[times]
+        trial_pFC = self.simulation.results().states()['FCepsilonRI/pFC'].values()[times] 
         ssq_pFC = math.sqrt(np.sum((pFC-trial_pFC)**2))
 
-        if(ssq_pFC < 0.1 and ssq_pSyk <0.1):
+        #if(ssq_pFC < 0.1 and ssq_pSyk <0.1):
             #print('Parameter set: ', self.constants)
-            ax1.plot(times,trial_pFC)
-            ax2.plot(times,trial_pSyk)
-            #plt.plot(ssq_pFC,ssq_pSyk,'*')
+            #print(parameter_values)
+            #ax1.plot(times,trial_pFC)
+            #ax2.plot(times,trial_pSyk)
+            #ax3.plot(ssq_pFC,ssq_pSyk,'*')
             #print(ssq_pSyk, ssq_pFC)
         
         return([ssq_pSyk+ssq_pFC,ssq_pSyk, ssq_pFC])
@@ -99,17 +102,36 @@ class Simulation(object):
         for i, X in enumerate(self.samples):
             Y[i,0:3] = self.evaluate_ssq(X)
             Y[i,3:self.samples.shape[1]+3]=X
-        Z=np.sort(Y,axis = 0)
-		
-        print(Z)
-        
-        return Y
+        ind = np.argsort(Y[:,0])
+        Z=Y[ind]
+        return Z
+
+    def plot_n_best(self,n,param_sweep_results):
+        print(param_sweep_results[0:3,:])
+        for i in range(0,n):
+            self.simulation.clearResults()
+            self.simulation.resetParameters()
+            for j, k in enumerate(self.model_constants.keys()):
+                self.constants[k] = 10.0**param_sweep_results[i,j+3]
+            #print(param_sweep_results[i,j+3])
+            #print('Parameter set: ', self.constants)
+            self.simulation.run()
+            trial_pSyk = self.simulation.results().states()['FCepsilonRI/pSyk'].values()[times]
+            #print(trial_pSyk)
+            trial_pFC = self.simulation.results().states()['FCepsilonRI/pFC'].values()[times]
+            ax1.plot(times,trial_pFC)
+            ax2.plot(times,trial_pSyk)
+            ax3.plot(param_sweep_results[i,1],param_sweep_results[i,2],"*")
 
 plt.close('all')
-fig, (ax1, ax2) = plt.subplots(2, sharey=True)
+fig, (ax1, ax2,ax3) = plt.subplots(3, sharey=True)
 s = Simulation()
 
 v = s.run_parameter_sweep()
+s.plot_n_best(5,v)
+
+
 ax1.plot(times,pFC,'*')
 ax2.plot(times,pSyk,'*')
+
 plt.show()
